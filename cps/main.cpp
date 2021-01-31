692bb6a9161a7646e3bd763e48350719b5aea348
yansong.li <liyansong@vackbot.com>
2021-01-31 17:51:49 +0800

    style

--- cps/main.cpp
+++ cps/main.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 using namespace std;
 
-void for_print(){
-    int v[] = {0,1,2,3,4,5,6,7,8,9};
+void for_print()
+{
+    int v[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-    for(auto x: v){
+    for (auto x : v)
+    {
         cout << x << '\n';
     }
 
-    for(auto x: {10,11,12}){
+    for (auto x : {10, 11, 12})
+    {
         cout << x << '\n';
     }
-
 }
-int count_x(char* p, char x){
-    if (p==nullptr) return 0;
-    int count=0;
+int count_x(char *p, char x)
+{
+    if (p == nullptr)
+        return 0;
+    int count = 0;
     // 用for循环
     // for (; p!=nullptr; ++p){
     //     if (*p==x){
@@ -23,8 +27,10 @@ int count_x(char* p, char x){
     //     }
     // }
     // 用while循环
-    while(p){
-        if (*p==x){
+    while (p)
+    {
+        if (*p == x)
+        {
             ++count;
         }
         ++p;
@@ -32,40 +38,83 @@ int count_x(char* p, char x){
     return count;
 }
 
-// struct Point
-// {
-//     /* data */
-//     int x, y;
-//     int scale;
-// };
+struct Point
+{
+    /* data */
+    int x, y;
+    int scale;
+};
 
-// void move_x(int x){}
-// void update_display(){}
+void move_x(int x) {}
+void update_display() {}
 
-// void action(){
-//     while (true){
-//         cout << "enter action: \n";
-//         string act;
-//         cin >> act;
-//         Point delta {0,0};
-//         for(char ch: act){
-//             switch(ch){
-//                 case 'u':
-//                 case 'n':
-//                     ++delta.y;
-//                     break;
-//                 default:
-//                     cout << "I freeze!\n";
-//             }
-//             int current {1};
-//             int scale {0};
-//             move(current+delta.scale);
-//             update_display();
-//         }
-//     }
-// }
+void action()
+{
+    while (true)
+    {
+        cout << "enter action: \n";
+        string act;
+        cin >> act;
+        Point delta{0, 0};
+        for (char ch : act)
+        {
+            switch (ch)
+            {
+            case 'u':
+            case 'n':
+                ++delta.y;
+                break;
+            default:
+                cout << "I freeze!\n";
+            }
+            int current{1};
+            int scale{0};
+            move(current + delta.scale);
+            update_display();
+        }
+    }
+}
 
-int main(){
+enum class Color
+{
+    red,
+    green,
+    blue
+};
+enum class Color_Light
+{
+    green,
+    yellow,
+    red
+};
+
+void enum_demo()
+{
+    Color v = Color::red;
+    Color_Light s = Color_Light::green;
+}
+Color_Light light = Color_Light::green;
+Color_Light &operator++(Color_Light &t)
+{
+    switch (t)
+    {
+    case Color_Light::green:
+        t = Color_Light::green;
+    case Color_Light::yellow:
+        t = Color_Light::yellow;
+    case Color_Light::red:
+        t = Color_Light::red;
+    }
+    return t;
+}
+
+int main()
+{
     cout << "Hello world!\n";
     for_print();
+    cout << "====================" << endl;  // TODO 不明白
+    std::cout << static_cast<int>(light) << std::endl;
+    Color_Light next = ++light;
+    std::cout << static_cast<int>(next) << std::endl;
+    return 0;
 }
